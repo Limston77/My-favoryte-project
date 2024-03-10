35ff617a2053ec2d50e84a5553fd70c1cebaa6fc
Albert <vip.khapakov@mail.com>
2024-03-10 23:14:18 +0300

    Изменил прозрачность в оглавлении

---
+++ src/Components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+        <div>
+            <span className='logo'>Rent-Bike</span>
+            <ul className='nav'>
+              <li>Личный кабинет</li>
+              <li>Контакты</li>
+              <li>О нас</li>
+            </ul>
+        </div>
+        <div className='Presentation'></div>
+    </header>
+
+    
+  )
+}
